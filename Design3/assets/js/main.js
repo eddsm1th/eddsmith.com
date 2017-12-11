10a0996692093cb104b56775768c72a8146795ed
Edd Smith <edd@bigbrandideas.co.uk>
2017-12-11 17:28:03 +0000

    skews

--- Design3/assets/js/main.js
+++ Design3/assets/js/main.js
@@ -6,7 +6,10 @@ $(document).ready(function(){
 
 
 	  	// X and Y movement
-	    $('.paralax').css('transform','translate(' + Xoffset*-2 + 'px, ' + Yoffset*-2 + 'px) rotateX(' + Yoffset*2 + 'deg) rotateY(' + Xoffset*-2 + 'deg)');
+	    $('.paralax-js').css('transform','translate(' + Xoffset*-2 + 'px, ' + Yoffset*-2 + 'px) rotateX(' + Yoffset*2 + 'deg) rotateY(' + Xoffset*-2 + 'deg)');
+
+	     $('.skew-js').css('transform','skewY(' + Xoffset*0.2 + 'deg) translateY(-13%)');
+	     $('.skew-reverse-js').css('transform','skewY(' + Xoffset*-0.2 + 'deg)');
 	})
 
 	function get_offset(mouse, browser){
