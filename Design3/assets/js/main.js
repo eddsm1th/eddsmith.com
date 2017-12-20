2ad03c2625b71c3b175ef094f90c6eed5bb7e327
Edward Smith <Edd@Edwards-MacBook-Pro.local>
2017-12-20 10:32:49 +0000

    did some things to the things yano? cool? cool.

--- Design3/assets/js/main.js
+++ Design3/assets/js/main.js
@@ -2,9 +2,9 @@ $(document).ready(function(){
 
 	$('.js-overlay-title').css('transform','translateY(-' + $('.js-overlay-title').height()/2 + 'px)')
 
-	$(window).mousemove(function(){
-	    var Xoffset = get_offset(event.clientX, $( window ).width())
-	    var Yoffset = get_offset(event.clientY, $( window ).height())
+	$('.js-header').mousemove(function(){
+	    var Xoffset = get_offset(event.clientX, $(this).width())
+	    var Yoffset = get_offset(event.clientY, $(this).height())
 
 	  	// X and Y movement
 	    $('.header-paralax-js').css('transform','translate(' + Xoffset*-4 + 'px, ' + Yoffset*-4 + 'px) rotateX(' + Yoffset*2 + 'deg) rotateY(' + Xoffset*-2 + 'deg)');
