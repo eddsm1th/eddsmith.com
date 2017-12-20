17a26f189edaa457bb6b8f5eae838dfdfc0d0803
Edward Smith <Edd@Edwards-MacBook-Pro.local>
2017-12-20 10:32:38 +0000

    did some things to the things yano? cool? cool.

--- Design3/assets/js/main.js
+++ Design3/assets/js/main.js
@@ -6,16 +6,20 @@ $(document).ready(function(){
 	    var Xoffset = get_offset(event.clientX, $( window ).width())
 	    var Yoffset = get_offset(event.clientY, $( window ).height())
 
-
 	  	// X and Y movement
-	    $('.paralax-js').css('transform','translate(' + Xoffset*-4 + 'px, ' + Yoffset*-4 + 'px) rotateX(' + Yoffset*2 + 'deg) rotateY(' + Xoffset*-2 + 'deg)');
+	    $('.header-paralax-js').css('transform','translate(' + Xoffset*-4 + 'px, ' + Yoffset*-4 + 'px) rotateX(' + Yoffset*2 + 'deg) rotateY(' + Xoffset*-2 + 'deg)');
 
-	     $('.skew-js').css('transform','skewY(' + Xoffset*0.2 + 'deg)');
-	     $('.skew-reverse-js').css('transform','skewY(' + Xoffset*-0.2 + 'deg)');
+	    $('.header-skew-js').css('transform','skewY(' + Xoffset*0.2 + 'deg)');
+	    $('.header-skew-reverse-js').css('transform','skewY(' + Xoffset*-0.2 + 'deg)');
 	})
 
 	function get_offset(mouse, browser){
 		return (((mouse - (browser / 2))/(browser / 2))*10) * -1;
 	}
+
+	$(window).scroll(function(){
+		$('.main-layer-overlay').css('height','calc(50% - ' + ($(document).scrollTop() / 4) + 'px)');
+		$('.js-overlay-title').css('transform','translateY(-' + ( ( $('.js-overlay-title').height()/2 ) - ( $(document).scrollTop() / 4 ) ) + 'px)');
+	})
 
 })
