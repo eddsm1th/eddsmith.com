744b46ed635203d99127fd9f38d630da6cb3f8a8
Edd Smith <edd@bigbrandideas.co.uk>
2017-12-21 09:01:09 +0000

    things

--- Design3/assets/js/main.js
+++ Design3/assets/js/main.js
@@ -1,14 +1,16 @@
 $(document).ready(function(){
 
-	$('.js-overlay-title').css('transform','translateY(-' + $('.js-overlay-title').height()/2 + 'px)')
+	$('.js-overlay-content-wrapper').css('margin-top','-' + $('.js-overlay-content-wrapper').height()/2 + 'px')
 
-	$(window).mousemove(function(){
-	    var Xoffset = get_offset(event.clientX, $( window ).width())
-	    var Yoffset = get_offset(event.clientY, $( window ).height())
+	$('header').mousemove(function(){
+	    var Xoffset = get_offset(event.clientX, $(this).width())
+	    var Yoffset = get_offset(event.clientY, $(this).height())
 
+	    console.log('Xoffset: ' + Xoffset + '    Yoffset: ' + Yoffset);
 
 	  	// X and Y movement
-	    $('.paralax-js').css('transform','translate(' + Xoffset*-4 + 'px, ' + Yoffset*-4 + 'px) rotateX(' + Yoffset*2 + 'deg) rotateY(' + Xoffset*-2 + 'deg)');
+	    
+	   $('.paralax-js').css('transform','translate(' + Xoffset*-6 + 'px, ' + Yoffset*-4 + 'px) rotateX(' + Yoffset*2 + 'deg) rotateY(' + Xoffset*-2 + 'deg)');
 
 	     $('.skew-js').css('transform','skewY(' + Xoffset*0.2 + 'deg)');
 	     $('.skew-reverse-js').css('transform','skewY(' + Xoffset*-0.2 + 'deg)');
