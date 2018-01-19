e241f89458457cc87ae34af6599c38a5eed422ff
Edd Smith <edd@bigbrandideas.co.uk>
2018-01-19 16:26:59 +0000

    new things :)

--- Design3/assets/js/main.js
+++ Design3/assets/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-	$('.js-overlay-content-wrapper').css('margin-top','-' + $('.js-overlay-content-wrapper').height()/2 + 'px')
+	$('.js-overlay-title').css('margin-top','-' + $('.js-overlay-title').height()/2 + 'px')
 
 
 	$('.js-header').mousemove(function(){
@@ -16,6 +16,35 @@ $(document).ready(function(){
 
 	function get_offset(mouse, browser){
 		return (((mouse - (browser / 2))/(browser / 2))*10) * -1;
+	}
+
+
+
+	var max_num = 100;
+	var counter = 0;
+	var const_line = max_num/2;
+	var triNum = triNumCalc(const_line);
+	var delay = triNum;
+	var trueDelay = 0;
+
+	counter_js();
+
+	function counter_js(){
+
+		$('.counter-js').html(counter.toString());
+		delay -= counter;
+		trueDelay = triNum - delay;
+		counter ++;
+
+		if ( counter <= max_num ) {
+			setTimeout(function(){
+				counter_js();
+			}, trueDelay*0.04)	
+		}
+	}
+
+	function triNumCalc(varNum){
+		return ( varNum * (varNum + 1 ) ) / 2; 
 	}
 
 })
