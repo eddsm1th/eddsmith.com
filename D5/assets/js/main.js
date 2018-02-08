6f0ad40530ec1036505cdc5a97bf498e6e29f855
Edd Smith <edd@bigbrandideas.co.uk>
2018-02-08 15:31:35 +0000

    did things

--- D5/assets/js/main.js
+++ D5/assets/js/main.js
@@ -39,7 +39,7 @@ $(document).ready(function(){
 
 	function view_on_scroll(){
 		$('.js-view-on-scroll').each(function(){
-			if ( ( ( $(this).offset().top - $(window).scrollTop() ) - window_height ) <= 0 ) {
+			if ( ( ( $(this).offset().top - $(window).scrollTop() ) - window_height ) < 0 ) {
 				$(this).addClass('in-view');
 			} else {
 				$(this).removeClass('in-view');
