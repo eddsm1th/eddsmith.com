d21521d45f424a3f08cde329d451d454f9518a35
Edd Smith <edd@bigbrandideas.co.uk>
2018-02-08 12:14:44 +0000

    added some swanky animations

--- D5/assets/js/main.js
+++ D5/assets/js/main.js
@@ -1,8 +1,50 @@
 $(document).ready(function(){
 
-	$('.nav-left').click(function(){ 
-		$(this).toggleClass('active');
-		$('.nav-right').toggleClass('active'); 
+	var window_height = window.innerHeight;
+	
+	view_on_scroll();
+
+	$('.nav-left').click(function(){
+		if ( $(this).hasClass('can-click') ) {
+
+			$(this).toggleClass('active').removeClass('can-click');
+			
+			if ( $('.nav-right').hasClass('active') ) {
+				$('.nav-right').find('li').removeClass('open');
+				$('.nav-right').delay(400).queue(function(){
+					$(this).removeClass('active').dequeue();	
+				})
+			} else {
+				$('.nav-right').addClass('active');
+				setTimeout(function(){
+					nav_transition(1);
+				}, 700)
+			}
+
+			setTimeout(function(){ $('.nav-left').addClass('can-click'); }, 1400);
+		}
 	})
+
+	function nav_transition(index){
+		$('li:nth-child(' + index + ')').addClass('open')
+
+		if ( index <= 4 ) {
+			setTimeout(function(){
+				nav_transition(index + 1);
+			}, 80)
+		}
+	}
+
+	$(window).scroll(function(){ view_on_scroll(); })
+
+	function view_on_scroll(){
+		$('.js-view-on-scroll').each(function(){
+			if ( ( ( $(this).offset().top - $(window).scrollTop() ) - window_height ) <= 0 ) {
+				$(this).addClass('in-view');
+			} else {
+				$(this).removeClass('in-view');
+			}
+		})
+	}
 
 })
